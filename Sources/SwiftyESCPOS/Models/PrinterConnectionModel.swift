19ca0a0b14f4569abb363948c4eea007f575f1b2
Alpatyev Nikita <118742566+alpatyev@users.noreply.github.com>
2023-06-30 01:20:31 +0300

    again access levels...

--- Sources/SwiftyESCPOS/Models/PrinterConnectionModel.swift
+++ Sources/SwiftyESCPOS/Models/PrinterConnectionModel.swift
@@ -6,7 +6,13 @@ public struct PrinterConnectionModel: Hashable {
     public let id = UUID()
     public let host: String
     public let port: String 
-    public var state: ConnectionState = .disconnected
+    public var state: ConnectionState
+    
+    public init(host: String, port: String) {
+        self.host = host
+        self.port = port
+        self.state = .disconnected
+    }
 }
 
 // MARK: - Connection state
