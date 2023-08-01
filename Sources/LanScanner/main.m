a7e190aa4daadb33904f08418954b7c80976f630
Alpatyev Nikita <118742566+alpatiev@users.noreply.github.com>
2023-08-01 21:10:31 +0300

    added lanscanner files

---
+++ Sources/LanScanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LAN Scan
+//
+//  Created by Mongi Zaidi on 24 February 2014.
+//  Copyright (c) 2014 Smart Touch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
